4497db50e150a2a30420553ab2b7432898d15a14
MINECRAFTBUILDHAX1 <mmajcherczykolczak@gmail.com>
2025-04-28 09:38:21 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'flick-bounce-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/game.html',
+  '/game.js',
+  '/styles.css',
+  '/manifest.json',
+  '/og-image.png',
+];
+
+// Install the service worker and cache necessary assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate the service worker and clear old caches if necessary
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch assets from cache first, then network if not cached
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);
+      })
+  );
+});
